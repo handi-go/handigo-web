6cb1760f9e9494eea45cec0e893ba7ceca0dda90
Philip-21 <philipuzomaobiora@gmail.com>
2026-01-30 22:53:30 +0100

    db queries fix
    Signed-off-by: Philip-21 <philipuzomaobiora@gmail.com>

--- server/src/handigo_service/interface/api/v1/dto/user.py
+++ server/src/handigo_service/interface/api/v1/dto/user.py
@@ -1,13 +1,28 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel
+from typing import List
+from uuid import UUID
+from handigo_service.application.model import Role
 
 
-class CustomerRegistrationRequest(BaseModel):
-    email: EmailStr
+class UserRegistrationRequest(BaseModel):
+    email: str
     password: str
+
     first_name: str | None = None
     last_name: str | None = None
     phone: str | None = None
 
+    is_customer: bool = False
+    is_artisan: bool = False
+
+
+class UserRegistrationResponse(BaseModel):
+    uuid: UUID
+    email: str
+    roles: List[Role]
+
+    class Config:
+        orm_mode = True
 
 class Token(BaseModel):
     access_token: str
--- server/src/handigo_service/interface/api/v1/routes/route_auth.py
+++ server/src/handigo_service/interface/api/v1/routes/route_auth.py
@@ -1,16 +1,26 @@
+import logging
 from datetime import timedelta
 from typing import Annotated
+
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
 from starlette import status
+
 from handigo_service.config import ServerAPIConfig
 from handigo_service.dependancy_container import Application
 from handigo_service.interface.api.auth import create_access_token
-from handigo_service.interface.api.v1.dto.user import Token
+from handigo_service.interface.api.v1.dto.user import (
+    Token,
+    UserRegistrationRequest,
+    UserRegistrationResponse
+)
+
+
+logger = logging.getLogger(__name__)
 
 router = APIRouter(prefix="/v1/auth", tags=["Auth"])
 
-@router.post("/token", response_model=Token)
+@router.post("/login", response_model=Token)
 async def login_for_access_token(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
     application: Application = Depends(Application),
@@ -19,10 +29,11 @@ async def login_for_access_token(
         email=form_data.username,
         password=form_data.password,
     )
+
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
+            detail="Incorrect email or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
 
@@ -33,3 +44,16 @@ async def login_for_access_token(
     )
 
     return {"access_token": token, "token_type": "bearer"}
+
+
+
+@router.post("/register", status_code=201, response_model=UserRegistrationResponse)
+async def register_user(
+    payload: UserRegistrationRequest,
+    application: Application = Depends(Application),
+):
+    try:
+        user = await application.auth.register_user(payload)
+        return user 
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
